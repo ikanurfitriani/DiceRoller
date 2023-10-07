3013d86d760b6befdeaae68f1b061a9ca9636fba
ikanurfitriani <ikan07393@gmail.com>
2023-10-07 07:38:14 +0700

    Update program dan penjelasan

--- app/src/main/java/com/ikanurfitriani/diceroller/MainActivity.kt
+++ app/src/main/java/com/ikanurfitriani/diceroller/MainActivity.kt
@@ -1,5 +1,7 @@
+// Nama package dari aplikasi yang dibuat
 package com.ikanurfitriani.diceroller
 
+// Import library yang akan digunakan
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -27,12 +29,16 @@ import com.ikanurfitriani.diceroller.ui.theme.DiceRollerTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Blok untuk menentukan tata letak aktivitas tempat fungsi composable
         setContent {
             DiceRollerTheme {
+                // A surface container using the 'background' color from the theme
                 Surface(
+                    // Untuk mengisi Surface dengan ukuran maksimum yang tersedia dalam konteksnya
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    // Memanggil fungsi utama yaitu DiceRollerApp dari aplikasi
                     DiceRollerApp()
                 }
             }
@@ -40,10 +46,15 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+// Menampilkan pratinjau aplikasi dice roller
 @Preview
+// Anotasi yang menandakan bahwa fungsi DiceRollerApp adalah komponen Composable
 @Composable
+// Fungsi utama yang akan membangun UI aplikasi "Dice Roller"
 fun DiceRollerApp() {
+    // Untuk menampilkan dadu dan tombol di tengah tampilan aplikasi
     DiceWithButtonAndImage(modifier = Modifier
+        // Untuk mengisi seluruh ruang yang tersedia dalam komposisi
         .fillMaxSize()
         .wrapContentSize(Alignment.Center)
     )
@@ -51,7 +62,9 @@ fun DiceRollerApp() {
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    // Untuk menyimpan hasil dari lemparan dadu
     var result by remember { mutableStateOf( 1) }
+    // Untuk menentukan gambar dadu yang akan ditampilkan berdasarkan nilai result
     val imageResource = when(result) {
         1 -> R.drawable.dice_1
         2 -> R.drawable.dice_2
@@ -60,13 +73,28 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
         5 -> R.drawable.dice_5
         else -> R.drawable.dice_6
     }
-    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        Image(painter = painterResource(imageResource), contentDescription = result.toString())
 
+    // Untuk mengatur elemen-elemen dalam tampilan secara vertikal
+    Column(
+        // Untuk meneruskan modifikasi yang diterima dari pemanggilan komponen DiceRollerApp
+        modifier = modifier,
+        // Untuk mengatur agar elemen-elemen dalam kolom berada di tengah secara horizontal
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // Untuk menampilkan gambar/foto
+        Image(
+            // Untuk menampilkan gambar dadu berdasarkan imageResource
+            painter = painterResource(imageResource),
+            contentDescription = result.toString()
+        )
+        // Button yang digunakan untuk membuat tombol "Roll"
         Button(
             onClick = { result = (1..6).random() },
         ) {
-            Text(text = stringResource(R.string.roll), fontSize = 24.sp)
+            // Teks tombol diatur sebagai "Roll" dengan ukuran font 24sp
+            Text(text = stringResource(R.string.roll),
+                fontSize = 24.sp
+            )
         }
     }
 }
